4101bd642a3fce7c80f14e1e6d0520ac5c8e2ff7
wth8ee <wth8eeyv2@gmail.com>
2026-07-06 00:34:38 +0300

    fixing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://mc.yandex.ru;
+  connect-src 'self' wss://mc.yandex.ru https://mc.yandex.ru;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data: https://mc.yandex.ru;
+`;
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: cspHeader,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
