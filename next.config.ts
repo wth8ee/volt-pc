220f7d0d9c3597cf3f3be961bc253575ecf612a7
wth8ee <wth8eeyv2@gmail.com>
2026-07-06 12:10:18 +0300

    nah

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,19 @@ import type { NextConfig } from "next";
 
 const cspDirectives = [
   "default-src 'self';",
-  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://mc.yandex.ru https://yookassa.ru https://yookassa.ru;",
-  "connect-src 'self' wss://mc.yandex.ru https://mc.yandex.ru https://*.yandex.ru https://*.yandex.az https://*.yandex.by https://*.yandex.co.il https://*.yandex.com https://*.yandex.com.am https://*.yandex.com.ge https://*.yandex.com.tr https://*.yandex.ee https://*.yandex.fr https://*.yandex.kg https://*.yandex.kz https://*.yandex.lt https://*.yandex.lv https://*.yandex.md https://*.yandex.tj https://*.yandex.tm https://*.yandex.uz https://*.sberbank.ru https://*.nspk.ru https://*.yoomoney.ru https://*.yoomoney.ru:* https://127.0.0.1:* https://api.bonuscake.ru https://yookassa.ru https://yookassa.ru;",
-  "style-src 'self' 'unsafe-inline' https://yookassa.ru https://yookassa.ru;",
-  "img-src 'self' blob: data: https://mc.yandex.ru https://yookassa.ru https://yookassa.ru;",
+  // Скрипты
+  "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://mc.yandex.ru https://yookassa.ru https://static.yookassa.ru;",
+  // Соединения (убрали дубли, оставили маску для локальных портов)
+  "connect-src 'self' wss://mc.yandex.ru https://mc.yandex.ru https://*.yandex.ru https://*.yandex.az https://*.yandex.by https://*.yandex.co.il https://*.yandex.com https://*.yandex.com.am https://*.yandex.com.ge https://*.yandex.com.tr https://*.yandex.ee https://*.yandex.fr https://*.yandex.kg https://*.yandex.kz https://*.yandex.lt https://*.yandex.lv https://*.yandex.md https://*.yandex.tj https://*.yandex.tm https://*.yandex.uz https://*.sberbank.ru https://*.nspk.ru https://*.yoomoney.ru https://*.yoomoney.ru:* https://127.0.0.1:* https://api.bonuscake.ru https://yookassa.ru https://api.yookassa.ru;",
+  // Стили ЮKassa
+  "style-src 'self' 'unsafe-inline' https://yookassa.ru https://static.yookassa.ru;",
+  // Изображения
+  "img-src 'self' blob: data: https://mc.yandex.ru https://yookassa.ru https://static.yookassa.ru;",
+  // Фреймы для 3D-Secure и окон оплаты
   "frame-src 'self' https://yookassa.ru https://*.sberbank.ru;",
 ];
 
+// Объединяем через пробел
 const cspHeader = cspDirectives.join(" ");
 
 const nextConfig: NextConfig = {
