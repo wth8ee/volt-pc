67db2e20deb3641a855a510a4a940d284c6b74b7
wth8ee <wth8eeyv2@gmail.com>
2026-07-09 21:49:15 +0300

    csp again

--- middleware.ts
+++ middleware.ts
@@ -9,14 +9,17 @@ export function middleware(request: NextRequest) {
     `script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-inline' https://yookassa.ru https://*.yoomoney.ru https://*.yandex.ru https://mc.yandex.ru ${process.env.NODE_ENV === "development" ? "'unsafe-eval'" : ""};`,
     "style-src 'self' 'unsafe-inline' https://yookassa.ru;",
     "connect-src 'self' https://yookassa.ru https://*.yoomoney.ru https://*.sberbank.ru https://*.yandex.ru wss://*.yoomoney.ru wss://*.sberbank.ru wss://*.yandex.ru wss://mc.yandex.ru wss://mc.yandex.ru/solid.ws;",
-    "frame-src 'self' https://yookassa.ru https://*.yoomoney.ru https://*.sberbank.ru https://*.paymentgate.ru https://*.yandex.ru;",
+    "frame-src 'self' https://yookassa.ru https://*.yoomoney.ru https://*.sberbank.ru https://*.paymentgate.ru https://*.yandex.ru wss://*.yandex.ru wss://mc.yandex.ru wss://mc.yandex.ru/solid.ws;",
     "child-src 'self' https://yookassa.ru https://*.yoomoney.ru https://*.yandex.ru;",
+
+    "worker-src 'self' blob: https://*.yandex.ru https://mc.yandex.ru ;",
+
     "img-src 'self' blob: data: https://yookassa.ru https://*.yoomoney.ru https://*.sberbank.ru https://*.yandex.ru;",
     "font-src 'self';",
     "object-src 'none';",
     "base-uri 'self';",
     "form-action 'self' https://yookassa.ru https://*.yoomoney.ru;",
-    "frame-ancestors 'self';",
+    "frame-ancestors 'none';",
     "upgrade-insecure-requests;",
   ];
 
